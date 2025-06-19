5d60dc5495a3301a3ca20e5d4eafff14a3ab906e
Bortolaci <joaopedrobortolaci2003@gmail.com>
2025-06-18 22:35:41 -0300

    aula 18/06/2025, caixa eletrônico

---
+++ aula18/script/caixaEletronico.js
@@ -0,0 +1,35 @@
+function withdrawMoney() {
+    document.getElementById("result").innerHTML = "";
+    let notas = [200, 100, 50, 20, 10, 5, 2, 1];
+    let money = parseInt(document.getElementById("withdraw").value);
+    if (!isNaN(money)) {
+        let notasUsadas = [];
+        while (money != 0) {
+            for (let i = 0; i < notas.length; i++) {
+                if(notas[i] <= money) {
+                    money -= notas[i];
+                    notasUsadas.push(notas[i]);
+                    break;
+                }
+            }
+            
+        }
+    
+        for (let i = 0; i < notas.length; i++) {
+            let contagem = 0;
+            while(notasUsadas.includes(notas[i])){
+                notasUsadas.splice(notasUsadas.indexOf(notas[i]), 1);
+                contagem++;
+            }
+            
+            if (contagem > 0) {
+                let item = document.createElement("li");
+                item.textContent = `${contagem} notas de R$${notas[i]}`;
+                let lista = document.getElementById('result');
+                lista.appendChild(item);
+            }
+        }
+    } else {
+        alert("Valor inserido não é um número");
+    }
+}
---
+++ aula18/script/script.js
@@ -0,0 +1,7 @@
+function trocarCorOver(cor) {
+    document.getElementById("mOver").style.backgroundColor = cor;
+}
+
+function trocarCorClick(cor) {
+    document.getElementById("mClick").style.backgroundColor = cor;
+}
